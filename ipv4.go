186dc8265e5ccf6b8b1829fbf46872c2e0612f5f
H0llyW00dzZ <priv8@btz.pm>
2024-03-24 03:49:40 +0700

    Implement IPv4 Range Verify (#1)
    - [+] feat(ipv4.go): add IPv4RangeVerify and singleIPv4ToUint32 functions
    - [+] test(ipv4_test.go): add tests for IPv4RangeVerify and singleIPv4ToUint32 functions

--- ipv4.go
+++ ipv4.go
@@ -6,6 +6,8 @@ package cidr
 
 import (
 	"net"
+	"strconv"
+	"strings"
 )
 
 // IPv4ToRange converts a CIDR block to a range of numeric IP addresses.
@@ -44,4 +46,63 @@ func IPv4ToRange(cidr *net.IPNet) (uint32, uint32) {
 	end := start | mask                      // Set the remaining bits to 1 to get the last address in the range.
 
 	return start, end
+}
+
+// IPv4RangeVerify checks if the given IP address falls within the given CIDR range.
+// It returns true if the IP is within the range, false otherwise.
+//
+// Parameters:
+//
+//	cidrBlock: A string representing the CIDR block.
+//	ipStr: A string representing the IP address to verify.
+//
+// Returns:
+//
+//	withinRange: A boolean indicating whether the IP is within the CIDR range.
+//	err: An error if there is an issue parsing the CIDR block or IP address.
+func IPv4RangeVerify(cidrBlock, ipStr string) (bool, error) {
+	_, cidrNet, err := net.ParseCIDR(cidrBlock)
+	if err != nil {
+		return false, err
+	}
+
+	start, end := IPv4ToRange(cidrNet)
+
+	ipNum, err := singleIPv4ToUint32(ipStr)
+	if err != nil {
+		return false, err
+	}
+
+	return ipNum >= start && ipNum <= end, nil
+}
+
+// singleIPv4ToUint32 converts an IPv4 address string to its uint32 representation.
+//
+// This function parses an IPv4 address in dotted-decimal notation ("192.168.1.1")
+// and converts it to a uint32 integer. Each octet of the IPv4 address is assumed
+// to be in the range of 0 to 255. The function returns an error if the string
+// is not a valid representation of an IPv4 address.
+//
+// Parameters:
+//
+// ipStr: A string representing the IPv4 address.
+//
+// Returns:
+//
+// ipNum: The uint32 representation of the IPv4 address.
+// err: An error if the string is not a valid IPv4 address.
+func singleIPv4ToUint32(ipStr string) (uint32, error) {
+	var ipNum uint32
+	bytes := strings.Split(ipStr, ".")
+	if len(bytes) != 4 {
+		return 0, net.InvalidAddrError("Invalid IP address format")
+	}
+	for _, b := range bytes {
+		p, err := strconv.ParseUint(b, 10, 8)
+		if err != nil {
+			return 0, err
+		}
+		ipNum = (ipNum << 8) + uint32(p)
+	}
+	return ipNum, nil
 }
